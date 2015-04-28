7f8fb525c9716c788d8083f4b7f97a40743e2477
John Anderson <sontek@gmail.com>
2015-04-28 19:54:14 +0200

    Beginning of the Transition Matrix implementation

--- src/main/java/com/isep/fst/MutableTransitionMatrix.java
+++ src/main/java/com/isep/fst/MutableTransitionMatrix.java
@@ -1,24 +1,63 @@
 package com.isep.fst;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
- * Created by Victor Delépine on 27/04/15.
+ * Created by devict on 27/04/15.
  */
 public class MutableTransitionMatrix {
 
+    private int lastState;
+    private HashMap<Integer, HashMap<Character, Integer>> matrix;
     private ArrayList<Character> letters;
 
-    public int getNumberOfStates() {
-        return 0;
+    public MutableTransitionMatrix(int initialState) {
+        this.lastState = initialState;
+        this.matrix = new HashMap<Integer, HashMap<Character, Integer>>();
     }
 
-    public int[] getStates() {
-        return new int[0];
+    public int addState() {
+        int newState = lastState + 1;
+        matrix.put(newState, new HashMap<Character, Integer>());
+        return newState;
+    }
+
+    public MutableTransitionMatrix addTransition(int source, char letter, int target) {
+        matrix.get(source).put(letter, target);
+        return this;
+    }
+
+    public int getTarget(int source, char letter) {
+        HashMap<Character, Integer> successors = matrix.get(source);
+        if (successors == null) {
+            throw new RuntimeException("Unknown source state : " + source);
+        }
+        return successors.get(letter);
+    }
+
+    public HashMap<Character, Integer> getTransitions(int source) {
+        return matrix.get(source);
+    }
+
+    public int getNumberOfTransitions() {
+        return matrix.values().stream().mapToInt(HashMap<Character,Integer>::size).sum(); // Java 8 FTW
     }
 
     public boolean hasState(int state) {
-        return false;
+        return matrix.containsKey(state);
+    }
+
+    public int getNumberOfStates() {
+        return matrix.keySet().size();
+    }
+
+    public int[] getStates() {
+        Integer[] states = (Integer[]) matrix.keySet().toArray();
+        int[] unboxedStates = new int[states.length];
+        for (int i = 0; i < states.length; i++) {
+            unboxedStates[i] = Integer.valueOf(states[i]);
+        }
+        return unboxedStates;
     }
 
     public ArrayList<Character> getLetters() {
---
+++ src/test/java/com/isep/fst/FSTTest.java
@@ -0,0 +1,11 @@
+package com.isep.fst;
+
+/**
+ * Created by Victor Delépine on 28/04/15.
+ */
+public class FSTTest {
+
+    public void testConstruction() {
+
+    }
+}
