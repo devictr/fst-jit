823aff569ba545330dce42a0c67f9cf1a4fb5729
Emilien Boulben <emilien.boulben@gmail.com>
2015-06-18 00:19:08 +0200

    FEATURE/MEDIUM: Generate Hello World in java

---
+++ java/first_test/HelloWorldGenerator.java
@@ -0,0 +1,36 @@
+class HelloWorldGenerator {
+
+    private static StringBuffer strBuff = new StringBuffer();
+
+    public static void main(String... args) {
+        append("class HelloWorld {");
+         
+        generateMain();
+
+        append("}");
+        System.out.print(strBuff.toString());
+    }
+
+    public static void append(String strToAppend) {
+        strBuff.append(strToAppend);
+        strBuff.append("\n");
+    }
+
+    public static void generateMain() {
+        appendWithTab("public static void main(String... args) {", 1);
+
+        appendWithTab("System.out.println(\"Hello World !\");", 2);
+
+        appendWithTab("}", 1);
+    }
+
+    public static void appendWithTab(String strToAppend, int numberOfTab) {
+        for (int i = 0; i < numberOfTab; i++) {
+            strBuff.append("\t");
+        }
+        strBuff.append(strToAppend);
+        strBuff.append("\n");
+    }
+
+
+}
