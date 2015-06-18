632f3d6a1b7be82dfad83c1ce222af76d17401a4
Emilien Boulben <emilien.boulben@gmail.com>
2015-06-18 15:58:03 +0200

    FEATURE/MINOR: Test all three generated class on given input

--- java/first_test/src/generator/Test.java
+++ java/first_test/src/generator/Test.java
@@ -1,9 +1,19 @@
 package generator;
 
+import generated.FstComputeTest1;
+import generated.FstComputeTest2;
+import generated.FstComputeTest3;
+
 class Test {
 
     public static void main (String[] args) {
-        String strInput = "TOP";
+        String strInput = "";
+
+        if(args.length > 0) {
+            strInput = args[0];
+        } else {
+            strInput = "MOTH";
+        }
 
         final int size = strInput.length();
         int[] input = new int[size];
@@ -12,8 +22,14 @@ class Test {
             input[i] = (int) strInput.charAt(i);
         }
 
-        float result = FstComputeSwitch.compute(input);
-        System.out.println(result);
+        float result1 = FstComputeTest1.compute(input);
+        float result2 = FstComputeTest2.compute(input);
+        float result3 = FstComputeTest3.compute(input);
+
+        System.out.println("test for word : " + strInput);
+        System.out.println("test1 -> " + result1);
+        System.out.println("test2 -> " + result2);
+        System.out.println("test3 -> " + result3);
     }
 
 }
