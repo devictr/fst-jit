ba30ef32105dbfdc7a6baa6157f8b43409bb1c38
Corentin Peuvrel <corentin.peuvrel@gmail.com>
2015-06-20 17:12:27 +0200

    FEATURE/MAJOR: Add a prototype for writing bytecode directly
    This code just create a class GenBCode with a method
    int run() that return 42

---
+++ java/src/main/java/generator/Gen.java
@@ -0,0 +1,34 @@
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.GeneratorAdapter;
+import org.objectweb.asm.commons.Method;
+
+public class Gen {
+
+    public static void main(String[] args) {
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        cw.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "GenBCode", null, "java/lang/Object", null);
+
+        Method init = Method.getMethod("void <init>()");
+        GeneratorAdapter gaInit = new GeneratorAdapter(Opcodes.ACC_PUBLIC, init, null, null, cw);
+        gaInit.visitCode();
+        gaInit.loadThis();
+        gaInit.invokeConstructor(Type.getType(Object.class), init);
+        gaInit.returnValue();
+        gaInit.endMethod();
+
+        Method run = Method.getMethod("int run()");
+        GeneratorAdapter ga = new GeneratorAdapter((Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC), run, null, null, cw);
+        ga.visitCode();
+        // BEGIN important part
+        ga.push(42);
+        // END important part
+        ga.returnValue();
+        ga.endMethod();
+
+        cw.visitEnd();
+
+        byte[] generatedByteCode = cw.toByteArray();
+    }
+}
